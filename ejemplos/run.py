7f0d9ed997979a237ab1b30cd0755d46fc710ede
Cristian Jimenez <116311718+CristianJ23@users.noreply.github.com>
2023-05-13 01:16:01 -0500

    Update run.py
    borrar

--- ejemplos/run.py
+++ ejemplos/run.py
@@ -1,32 +1,22 @@
 """
 
 """
-from mis_clases import Terreno
 
 # Crear dos objetos de la clase 01
 
 # objeto 01
 
 # crear
-terreno0 = Terreno(30, 20, 10)
 
 # Presentar objeto; usar el método __st__
-terreno0.calcularArea()
-terreno0.calcularCostoTerreno()
-print(terreno0.__str__())
+
 
 # objeto 02
 
 # crear ingresando valores por teclado
-ancho = float(input("Ingrese el ancho del terreno"))
-largo = float(input("Ingrese el largo del terreno"))
-valor = float(input("Ingrese el valor por metro cuadrado del terreno"))
 
-terreno1 = Terreno(ancho, largo, valor)
 
 # Presentar objeto; usar el método __st__
-terreno1.calcularArea()
-terreno1.calcularCostoTerreno()
-print(terreno1.__str__())
+
 
 
